88fbf0b4f6ef4d26fd5cfbe5ef30931c0511f625
fcamblor <fcamblor@gmail.com>
2016-09-05 18:13:49 +0200

    Removed every reference to bundle.min.js which is not produced anymore since 3a75ee55252afc6e49d0a40e9c298656c9313807 (npm run build doesn't create bundle.min.js file anymore)

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,4 @@
-# Generating js/bundle.min.js file
+# Generating js/bundle.js file
 npm run build
 
-# Replacing stuff for PRODUCTION
-echo "Replacing bundle.js reference into index.html..."
-sed -i "s/js\\/bundle.js/js\\/bundle.min.js/" index.html
 echo "=> Done !"
