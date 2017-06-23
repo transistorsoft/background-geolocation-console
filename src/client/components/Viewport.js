67b3f2a39ed6fcf9994b3be75ce36784fa55d3be
guevarasandoval <guevarasandoval@gmail.com>
2017-06-23 18:30:30 -0500

    q

--- src/client/components/Viewport.js
+++ src/client/components/Viewport.js
@@ -69,6 +69,9 @@ export default class Viewport extends Component {
           <Panel className={Styles.workspace} bodyScroll={false} scrollY={false}>
             <HeaderView />
             <Tabs index={this.state.activeTab} hideMode="display" onChange={this.handleTabChange.bind(this)} inverse >
+              <Tab label="Mapa" className={Styles.cabeceraTabla}>
+                <MapView />
+              </Tab>
               
             </Tabs>
           </Panel>
