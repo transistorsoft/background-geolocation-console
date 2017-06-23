0aca5dc4d63a90d4420475aafe980b20cb973e05
ZeusTheTrueGod <l-2732@yandex.ru>
2017-06-23 14:24:10 +0300

    forgot some files, as usually

---
+++ webpack.config.babel.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src', 'client'),
+  devtool: 'source-map',
+  target: 'web',
+  entry: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', 'react-hot-loader/patch', './app.js'],
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'app.bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.css', '.svg'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]--[local]--[hash:base64:8]',
+            },
+          },
+          'postcss-loader', // has separate config, see postcss.config.js nearby
+        ],
+      },
+    ],
+  },
+  plugins: [new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin()],
+};
