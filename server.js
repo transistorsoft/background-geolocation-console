5b021d49e0e3c4a3325dff98d1d3a13bd2cf6b14
Artur Dudnik <adudnik@gmail.com>
2019-09-12 22:20:14 +0300

    move @babel/core to dependencies

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ import initializeDatabase from './src/server/database/initializeDatabase';
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
 import compress from 'compression';
 import 'colors';
 import opn from 'opn';
@@ -30,6 +27,9 @@ process.on('uncaughtException', function (error) {
     app.use(express.static('./build'));
   } else {
     console.info('adding webpack');
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
     const webpackConfig = require('./webpack.config.babel');
     const compiler = webpack(webpackConfig);
 
