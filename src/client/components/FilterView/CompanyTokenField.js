be4647fc40de7a67eda0be5b1adf034739dcf92d
Artur Dudnik <adudnik@gmail.com>
2019-10-13 19:48:19 +0300

    transform company field to functional component

--- src/client/components/FilterView/CompanyTokenField.js
+++ src/client/components/FilterView/CompanyTokenField.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import { Select, MenuItem } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -44,173 +44,155 @@ const styles = (theme: any) => ({
   },
 });
 
-@withStyles(styles)
-class CompanyTokenField extends Component<Props> {
-  state = { open: false, filter: '' };
-
-  handleChange = (ev: Event) => {
-    this.scrollToIndex = 0;
-    this.setState({ scrollToIndex: 0, filter: ev.target.value })
-    ;
-  }
-
-  onErase = () => {
-    this.setState({ filter: '', scrollToIndex: 0 });
-  }
-
-  handleCancel = () => this.setState({ open: false });
-
-  handleOpen = () => {
-    if (!this.isLong) {
+const CompanyTokenField = withStyles(styles)((props: Props) => {
+  const { onChange, value, source: s, fullScreen, classes } = props;
+  const [dialogOpen, setOpen] = React.useState(false);
+  const [filter, setFilter] = React.useState('');
+  const handleChange = (ev: Event) => {
+    setFilter(ev.target.value);
+  };
+  const onErase = () => {
+    setFilter('');
+  };
+  const handleCancel = () => setOpen(false);
+  const handleOpen = () => {
+    if (!isLong) {
       return;
     }
-    setTimeout(
-      () => {
-        this.forceUpdate();
-        this.scrollerRef.forceUpdate();
-      },
-      500
-    );
-    this.setState({ open: true });
-  }
-
-  handleOk = (index: number) => {
-    this.setState({ open: false });
-    this.source.length && this.props.onChange(this.source[index].value);
+    setOpen(true);
   };
-
-  rowRenderer = ({ columnIndex, key, rowIndex, style }: any) => {
+  const handleOk = (index: number) => {
+    setOpen(false);
+    !!source.length && onChange(source[index].value);
+  };
+  const rowRenderer = ({ columnIndex, key, rowIndex, style }: any) => {
     return (
       <ListItem
         key={key}
-        onClick={() => this.handleOk(rowIndex)}
+        onClick={() => handleOk(rowIndex)}
         style={{ ...rowStyle, ...style }}
       >
         <ListItemIcon>
           <DeviceUnknownIcon />
         </ListItemIcon>
         <ListItemText>
-          {this.source[rowIndex].label}
+          {source[rowIndex].label}
         </ListItemText>
       </ListItem>
     );
   };
-
-  render () {
-    const { onChange, value, source, fullScreen, classes } = this.props;
-    const { open, filter } = this.state;
-    const val = filter.toLowerCase();
-    this.source = val ? source.filter((x: Source) => x.label && !!~x.label.toLowerCase().indexOf(val)) : source;
-    this.isLong = source.length > 2;
-
-    if (!filter && !this.source.length) {
-      return null;
-    }
-
-    return [
-      <Select
-        key='select'
-        autoWidth
-        open={this.isLong ? false : undefined}
-        onOpen={this.handleOpen}
-        style={flex}
-        label={`Users (${source.length})`}
-        onChange={({ target }: MaterialInputElement) => onChange(target.value)}
-        value={value}
-      >
-        {source.map((x: Source) => (<MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>))}
-      </Select>,
-      <Dialog
-        key='dialog'
-        // disableBackdropClick
-        // disableEscapeKeyDown
-        // onEntering={handleEntering}
-        aria-labelledby='filter-dialog-title'
-        aria-describedby='filter-dialog-description'
-        open={open}
-        fullWidth
-        maxWidth='sm'
-        fullScreen={fullScreen}
-        onClose={this.handleCancel}
-        scroll='paper'
-      >
-        <DialogTitle id='confirmation-dialog-title'>
-          Company selector
-          <IconButton aria-label='close' className={classes.closeButton} onClick={this.handleCancel}>
-            <CloseIcon />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent
-          dividers
-          style={contentStyle}
-        >
-          <RemoveAnimationProvider>
-            <FormControl>
-              <InputLabel htmlFor='filter'>Company Filter</InputLabel>
-              <Input
-                id='filter'
-                autoFocus
-                fullWidth
-                margin='dense'
-                value={filter}
-                onChange={this.handleChange}
-                endAdornment={
-                  <InputAdornment position='end'>
-                    <IconButton
-                      aria-label='toggle password visibility'
-                      onClick={this.onErase}
-                      onMouseDown={this.onErase}
-                    >
-                      <CloseRounded />
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-            <div style={containerStyle} ref={(ref: any) => (this.contentRef = ref)}>
-              <WindowScroller
-                scrollElement={this.contentRef}
-                ref={(ref: any) => (this.scrollerRef = ref)}
-              >
-                {({ isScrolling, registerChild, onChildScroll, scrollTop }: any) => (
-                  <AutoSizer>
-                    {({ width, height }: any) => (
-                      <div ref={registerChild}>
-                        <Grid
-                          // autoWidth
-                          autoContainerWidth
-                          autoHeight
-                          cellRenderer={this.rowRenderer}
-                          columnWidth={500}
-                          columnCount={1}
-                          height={height}
-                          overscanColumnCount={2}
-                          overscanRowCount={2}
-                          rowHeight={50}
-                          rowCount={this.source.length}
-                          scrollTop={scrollTop}
-                          width={width}
-                          isScrolling={isScrolling}
-                          onScroll={onChildScroll}
-                          filter={filter}
-                          // scrollToIndex={scrollToIndex}
-                        />
-                      </div>
-                    )}
-                  </AutoSizer>
-                )}
-              </WindowScroller>
-            </div>
-          </RemoveAnimationProvider>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={this.handleCancel} color='primary'>
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>,
-    ];
+  const isLong = s.length > 2;
+  const contentRef = React.createRef();
+  const val = filter.toLowerCase();
+  const source = val
+    ? s.filter((x: Source) => x.label && !!~x.label.toLowerCase().indexOf(val))
+    : s;
+  if (!s.length) {
+    return null;
   }
-}
+
+  return [
+    <Select
+      key='select'
+      autoWidth
+      open={isLong ? false : undefined}
+      onOpen={handleOpen}
+      style={flex}
+      label={`Companies (${s.length})`}
+      onChange={({ target }: MaterialInputElement) => onChange(target.value)}
+      value={value}
+    >
+      {s.map((x: Source) => (<MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>))}
+    </Select>,
+    <Dialog
+      key='dialog'
+      // disableBackdropClick
+      // disableEscapeKeyDown
+      // onEntering={handleEntering}
+      aria-labelledby='filter-dialog-title'
+      aria-describedby='filter-dialog-description'
+      open={dialogOpen}
+      fullWidth
+      maxWidth='sm'
+      fullScreen={fullScreen}
+      onClose={handleCancel}
+      scroll='paper'
+    >
+      <DialogTitle id='confirmation-dialog-title'>
+        Company selector
+        <IconButton aria-label='close' className={classes.closeButton} onClick={handleCancel}>
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent
+        dividers
+        style={contentStyle}
+      >
+        <RemoveAnimationProvider>
+          <FormControl>
+            <InputLabel htmlFor='filter'>Company Filter</InputLabel>
+            <Input
+              id='filter'
+              autoFocus
+              fullWidth
+              margin='dense'
+              value={filter}
+              onChange={handleChange}
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={onErase}
+                    onMouseDown={onErase}
+                  >
+                    <CloseRounded />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+          <div style={containerStyle} ref={contentRef}>
+            <WindowScroller
+              scrollElement={contentRef.current}
+            >
+              {({ isScrolling, registerChild, onChildScroll, scrollTop }: any) => (
+                <AutoSizer>
+                  {({ width, height }: any) => (
+                    <div ref={registerChild}>
+                      <Grid
+                        // autoWidth
+                        autoContainerWidth
+                        autoHeight
+                        cellRenderer={rowRenderer}
+                        columnWidth={500}
+                        columnCount={1}
+                        height={height}
+                        overscanColumnCount={2}
+                        overscanRowCount={2}
+                        rowHeight={50}
+                        rowCount={source.length}
+                        scrollTop={scrollTop}
+                        width={width}
+                        isScrolling={isScrolling}
+                        onScroll={onChildScroll}
+                        filter={filter}
+                        // scrollToIndex={scrollToIndex}
+                      />
+                    </div>
+                  )}
+                </AutoSizer>
+              )}
+            </WindowScroller>
+          </div>
+        </RemoveAnimationProvider>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel} color='primary'>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>,
+  ];
+});
 
 export default CompanyTokenField;
