2394e68b9baf27d2fb1dff633794da5de77717e0
Artur Dudnik <adudnik@gmail.com>
2019-11-28 09:43:09 +0300

    enpoint for error resons has been added /test/error/:status

--- src/server/routes/tests.js
+++ src/server/routes/tests.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
 // import { isEncryptedRequest, decrypt } from '../libs/RNCrypto';
-// import {
-//   AccessDeniedError,
-//   RegistrationRequiredError,
-// } from '../libs/utils';
+import {
+  AccessDeniedError,
+  RegistrationRequiredError,
+} from '../libs/utils';
 
 const router = new Router();
 
@@ -14,6 +14,19 @@ router.post('/test/locations/500', async function (req, res) {
 
 router.post('/test/register', async function (req, res) {
   res.status(200).send({ accessToken: 'Dummy access token' });
+});
+
+router.post('/test/error/:status', async function (req, res) {
+  const { status } = req.params;
+  res.status(+status).send({ message: 'Dummy error response' });
+});
+
+router.post('/test/error/403/AccessDeniedError', async function (req, res) {
+  res.status(403).send({ error: new AccessDeniedError('Dummy error').toString() });
+});
+
+router.post('/test/error/406/RegistrationRequiredError', async function (req, res) {
+  res.status(406).send({ error: new RegistrationRequiredError('Dummy error').toString() });
 });
 
 export default router;
