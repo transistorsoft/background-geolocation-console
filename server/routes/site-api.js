6d29f45103d50d1c5ffdf5d8f36f26426ce2a7a3
Chris Scott <christocracy@gmail.com>
2025-09-15 21:58:16 -0400

    Ban in /register

--- server/routes/site-api.js
+++ server/routes/site-api.js
@@ -194,7 +194,7 @@ router.post('/locations', getAuth(verify), async (req, res) => {
 router.post('/locations/:company_token', getAuth(verify), async (req, res) => {
   const { company_token: org } = req.params;
 
-  console.log('*** company_token: ', company_token, ', org: ', org);
+  console.log('*** params ', params);
 
   try {
     checkCompany(org, {});
