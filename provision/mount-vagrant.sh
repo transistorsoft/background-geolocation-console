31fb6bfeb159498f365a75d5dcab7768d26f393c
Dave Lazar <dlazar@resistorsoftware.com>
2026-01-11 13:42:22 -0500

    initial branch commit for Go code

--- provision/mount-vagrant.sh
+++
@@ -1,5 +0,0 @@
-set -e
-if [ ! -d /vagrant ]; then
-    mkdir /vagrant
-fi
-
--- server/firebase/check.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import check from './migration.js';
-
-check({ org: 'test', uuid: 'UNKNOWN' })
-  .then(() => process.exit(1));
